7dd33a1a5a2fa99d921631625660cbff80005056
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-18 22:01:31 +0530

    Add About Page component with header

--- app/about/page.js
+++ app/about/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      <h1 className='text-3xl underline'>Home Page</h1>
+      <h1 className='text-3xl underline'>About Page</h1>
     </div>
   );
 }
