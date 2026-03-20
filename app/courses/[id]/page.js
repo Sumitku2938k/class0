173f441d03187dfe9fce581e6cb3b9a42f7a07ea
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-20 11:41:08 +0530

    Update Course Page to display dynamic course ID from URL

--- app/courses/[id]/page.js
+++ app/courses/[id]/page.js
@@ -1,9 +1,13 @@
+'use client' // This is a client component, it can use hooks and other client side features
+import { useParams } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+    const { id } = useParams();
+    console.log(id);
     return (
         <div>
-            <h1 className='text-3xl underline'>XYZ Course</h1>
+            <h1 className='text-3xl underline'>{id} Course</h1>
         </div>
     );
 }
