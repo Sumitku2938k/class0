9c7db15500e0751cc025e69908ee66ec2134e5d5
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-20 11:27:37 +0530

    Enhance Product Page layout and add Men's Shirts component

---
+++ app/product/men/shirt/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='text-3xl underline'> 
+            <h1>Shirts for Men</h1>
+        </div>
+    );
+}
+
+export default page;
--- app/product/page.js
+++ app/product/page.js
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
   return (
     <div >
-      <div className='flex justify-center'>
+      <div className='flex justify-center gap-10 text-2xl font-bold mt-10'>
         <Link href="/product/men"> Men's Products</Link>
         <Link href="/product/women"> Women's Products</Link>
       </div>  
